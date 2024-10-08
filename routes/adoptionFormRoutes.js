23ae6a311e4e88d8aaceb2228b1fba7be358f4e3
unknown <mohamedelafandy593@gmail.com>
2024-10-08 11:19:04 -0700

    form protect

--- routes/adoptionFormRoutes.js
+++ routes/adoptionFormRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const adoptionFormController = require('./../controllers/adoptionFormController');
-
+const authController = require('./../controllers/authController');
 const router = express.Router();
 
 // Routes for CRUD operations
+router.use(authController.protect);
+
 router
     .route('/adoption')
     .get(adoptionFormController.getAllAdoptionForms)
