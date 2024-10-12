06a3bd57729bff6506ea088b0bd97ff80c226a91
unknown <mohamedelafandy593@gmail.com>
2024-10-12 11:01:10 -0700

    auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -37,34 +37,37 @@ const createSendToken = (user, statusCode, res) => {
 
 exports.protect = catchAsync(async (req, res, next) => {
     // 1) Getting token and check of it's there
-    if (!req.cookies || !req.cookies.jwt) {
-        return next(new AppError('You are not logged in! Please log in to get access.', 401));
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        token = req.headers.authorization.split(' ')[1]
+        if (!req.cookies || !req.cookies.jwt) {
+            return next(new AppError('You are not logged in! Please log in to get access.', 401));
+        }
+
+        const token = req.cookies.jwt;
+
+        // 2) Verification token
+        const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+        // 3) Check if user still exists
+        const currentUser = await Customer.findById(decoded.id);
+        if (!currentUser) {
+            return next(
+                new AppError(
+                    'The user belonging to this token does no longer exist.',
+                    401
+                )
+            );
+        }
+
+        if (currentUser.tokenInvalidationTime && currentUser.tokenInvalidationTime > decoded.iat * 1000) {
+            return next(new AppError('This token is no longer valid. Please log in again.', 401));
+        }
+
+        // GRANT ACCESS TO PROTECTED ROUTE
+        req.user = currentUser;
+        res.locals.user = currentUser;
+        next();
     }
-
-    const token = req.cookies.jwt;
-
-    // 2) Verification token
-    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-
-    // 3) Check if user still exists
-    const currentUser = await Customer.findById(decoded.id);
-    if (!currentUser) {
-        return next(
-            new AppError(
-                'The user belonging to this token does no longer exist.',
-                401
-            )
-        );
-    }
-
-    if (currentUser.tokenInvalidationTime && currentUser.tokenInvalidationTime > decoded.iat * 1000) {
-        return next(new AppError('This token is no longer valid. Please log in again.', 401));
-    }
-
-    // GRANT ACCESS TO PROTECTED ROUTE
-    req.user = currentUser;
-    res.locals.user = currentUser;
-    next();
 });
 
 
